c5bdc553965d6e7604a32688a4b5f7ac3eb0a0e6
Bhavya-Hingorani <bhavyahingorani54@gmail.com>
2025-03-27 21:47:50 +0530

    Definition of vitalState updated

--- Entities/Enums/ApiVitalsState.cs
+++ Entities/Enums/ApiVitalsState.cs
@@ -2,8 +2,8 @@ namespace ApiBroker.Entities.Enum
 {
     /*
         API can be one of the 3 states mentioned below
-        RED - Rate limit reached and we absolutely cannot call the api
-        ORANGE - Latency is too high, Response time too high
+        RED - Rate limit reached and we absolutely cannot call the api or error rate is way too high
+        ORANGE - Callable but Latency is too high or Response time too high
         GREEN - All is good and we can call it normally
     */
     public enum ApiVitalsState
